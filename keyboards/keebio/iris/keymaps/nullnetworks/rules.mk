f8844332e7d4b27f6d1ab815e4a169a7159cc114
Jeremy Ehrlich <nullard@gmail.com>
2023-01-07 18:32:42 +0200

    Keymap def added, WHEN_HELD macro added, combos not configured yet

---
+++ keyboards/keebio/iris/keymaps/nullnetworks/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE = no            # VIA gui configurator support
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = no
+DYNAMIC_TAPPING_TERM_ENABLE = no
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+
+SPLIT_KEYBOARD = yes
+ENCODER_ENABLE = no
+ENCODER_MAP_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+LTO_ENABLE = yes
+
+CONSOLE_ENABLE = yes
